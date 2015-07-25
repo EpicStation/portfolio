910f4cc88635d82d352ae6988f5d1a5942a3c7da
EpicStation <y.boyko.team@gmail.com>
2015-07-25 19:38:36 +0300

    all web

---
+++ formdata.php
@@ -0,0 +1,10 @@
+<?php
+echo "<p>Ім'я: $_POST[name]</p>";
+echo "<p>Пароль: $_POST[password]</p>"; 
+echo "<p>Велороверист? $_POST[ask]</p>";
+echo "<p>Блокнот: $_POST[bl]</p>";
+echo "<p>Щоденник: $_POST[sh]</p>"; 
+echo "<p>Зошит: $_POST[zo]</p>";
+echo "<p>Скільки годин на день ви спите?: $_POST[sleep]</p>";
+echo "<p>Напишіть про себе: $_POST[text]</p>";
+?>
